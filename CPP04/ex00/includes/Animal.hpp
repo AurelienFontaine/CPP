73522b352cec1dbf5f2e51eff12be1e385b185ba
Aurelien FONTAINE <afontain@made-f0Dr9s17.clusters.42paris.fr>
2024-11-28 12:53:37 +0100

    create ex

---
+++ CPP04/ex00/includes/Animal.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afontain <afontain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 12:20:57 by afontain          #+#    #+#             */
+/*   Updated: 2024/11/28 12:22:47 by afontain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <string.h>
+# include <iostream>
+
+class Animal
+{
+	protected :
+		std::string type;
+	public :
+
+};
+
+#endif
