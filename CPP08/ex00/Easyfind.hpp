3d46fa60eff931d47c398cb0fec0fa4d48978b33
AurelienFontaine <a.fontaine64@laposte.net>
2025-02-21 18:56:14 +0100

    debut 08

---
+++ CPP08/ex00/Easyfind.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afontain <afontain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 18:17:21 by afontain          #+#    #+#             */
+/*   Updated: 2025/02/21 18:23:50 by afontain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+
+template <typename T>
+
+class Easyfind
+{
+    private :
+
+    public :
+        Easyfind();
+        ~Easyfind();
+};
+
+
+
+#endif
